c44b2dc546fa9900613a6e72911bd6bed0737964
Weslley Cutrim <weslleycga@gmail.com>
2026-08-21 10:41:21 -0300

    adicionando o exercicio invertendo nome

---
+++ Desafios parte 2/Nome_Invertido.py
@@ -0,0 +1,4 @@
+# Nome invertido
+#desafio 6 disciplina de python
+nome = str(input("Digite seu nome: ")) #recebe o nome digitado pelo usuario
+print("Nome invertido:", nome[::-1]) #exibe o nome invertido   
