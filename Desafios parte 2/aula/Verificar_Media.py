769db12a66557053877832823b430e302b33455d
Weslley Cutrim <weslleycga@gmail.com>
2026-08-24 16:22:28 -0300

    adicionando condição de recuperação

--- Desafios parte 2/aula/Verificar_Media.py
+++ Desafios parte 2/aula/Verificar_Media.py
@@ -2,5 +2,7 @@ n1, n2 , n3 = map(float, input("Digite as três notas separadas por espaço: ").
 media = (n1 + n2 + n3) / 3
 if media >= 7:
     print("Aprovado! Média:", media)
+elif media >= 5:
+    print("Recuperação! Média:", media)
 else:
     print("Reprovado! Média:", media)
