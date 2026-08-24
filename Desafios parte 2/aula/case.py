9086f4237a2b4224473f3eb965095f88b4369982
Weslley Cutrim <weslleycga@gmail.com>
2026-08-24 16:37:56 -0300

    adicionando exercicio de case

---
+++ Desafios parte 2/aula/case.py
@@ -0,0 +1,8 @@
+print("Escolha uma das opções abaixo:")
+print("1. Opção 1")
+print("2. Opção 2")
+print("3. Opção 3")
+
+opcao = int(input("Digite o número da opção desejada: "))
+
+print("Você escolheu a opção:", opcao)
